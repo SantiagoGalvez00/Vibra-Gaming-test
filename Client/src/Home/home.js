3685a1413cb6dde098a2cb74533fd7e8d4598bb7
SantiagoGalvez00 <galvez.santiago.92@gmail.com>
2022-10-04 21:14:37 -0300

    Layout Home y boostrap config

--- Client/src/Home/home.js
+++ Client/src/Home/home.js
@@ -5,24 +5,63 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            table: false
+        };
     }
 
     async onclick() {
-        const response = await axios.get('/rest/search',{
+        const response = await axios.get('/rest/search', {
             params: {
                 name: 'KimberlyTilliards',
                 city: 'Živinice',
                 quantity: 2
             }
         });
-        console.log({response});
+        console.log({ response });
+        this.setState({
+            table: true
+        })
     }
 
     render() {
+
         return (
             <>
-                <button type='button' onClick={async () => this.onclick()}>Boton</button>
+                <div className="container">
+
+                    <nav className="navbar navbar-dark bg-dark">
+                        <div className="container-fluid">
+                            <a className="navbar-brand" href="/">Navbar</a>
+                        </div>
+                    </nav>
+
+                    <div style={{ marginTop: '50px' }}>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        </div>
+
+                        {this.state.table ?
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Tabla</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1" />
+                            </div>
+
+                            : <button type='button' onClick={async () => this.onclick()}>Boton</button>
+                        }
+                    </div>
+
+                </div>
             </>
         )
     }
--- Client/src/index.js
+++ Client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Home from './Home/home'
 import axios from 'axios';
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import "../src/index.css";
+
+
 axios.defaults.baseURL = 'http://localhost:5000';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
